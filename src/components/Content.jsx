27bce6bf0bd89e80c1380672ce698e694705431f
rraj-official <119522203+rraj-official@users.noreply.github.com>
2023-06-13 11:43:45 +0530

    sidescroll bug fixed

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,8 +8,8 @@ export default function Content(prop) {
         <div className="absolute inset-0 -z-10 overflow-hidden">
 
         </div>
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+        <div className="mx-auto grid max-w-full grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:items-start lg:gap-y-10">
+          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-full lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="lg:max-w-lg">
                 <h1 id='about' className="pt-8 text-3xl font-bold tracking-tight text-theme-color sm:text-4xl">About</h1>
